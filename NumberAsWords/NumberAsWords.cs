eaafa8d42b9a2c61ebcda8d94d587a50bb2fda74
borislavnivanov <borislavnivanov@gmail.com>
2016-06-11 01:05:38 +0300

    update

--- NumberAsWords/NumberAsWords.cs
+++ NumberAsWords/NumberAsWords.cs
@@ -20,11 +20,112 @@ numbers number as words
 
 using System;
 
-
-class NumberAsWords
+namespace NumbersAsWords
 {
-    static void Main()
-    {
 
+    class NumberAsWords
+    {
+        static byte number = 0;
+        static string[] numsToText = { "zero", "one", "two", "tree", "four", "five", "six", "seven", "eight", "nine" };
+        static string[] teensToText = { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        static string[] tensToText = { "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+        static string userInput;
+
+        static void Main()
+        {
+            Console.WriteLine("Please enter number between 0 and 999:");
+            userInput = Console.ReadLine();
+
+            try
+            {
+                number = byte.Parse(userInput);
+            }
+            catch
+            {
+                //TODO: remove the line from showing 
+                Console.WriteLine("Pleae enter a valid number between 0 and 999.");
+            }
+
+            if (userInput.Length == 1)
+            {
+                Console.WriteLine(numsToText[number]);
+            }
+            else if (userInput.Length == 2)
+            {
+                twoNumbers();
+            }
+            else if (userInput.Length == 3)
+            {
+                treeNumbers();
+            }
+        }
+        static void twoNumbers()
+        {
+            char n = userInput[0];
+            string p = char.ToString(n);
+            byte firstNum = byte.Parse(p);
+
+            char y = userInput[1];
+            string x = char.ToString(y);
+            byte secondNum = byte.Parse(x);
+
+            if (firstNum == 1)
+            {
+                Console.WriteLine(teensToText[secondNum]);
+            }
+            else
+            {
+                if (secondNum == 0)
+                {
+                    Console.WriteLine(tensToText[firstNum - 2]);
+                }
+                else
+                {
+                    Console.WriteLine("{0} {1}", tensToText[firstNum - 2], numsToText[secondNum]);
+                }
+            }
+        }
+
+        static void treeNumbers()
+        {
+            char n = userInput[0];
+            string p = char.ToString(n);
+            byte firstNum = byte.Parse(p);
+
+            char y = userInput[1];
+            string x = char.ToString(y);
+            byte secondNum = byte.Parse(x);
+
+            char r = userInput[2];
+            string g = char.ToString(r);
+            byte thirdNum = byte.Parse(g);
+
+            if (secondNum == 1)
+            {
+                Console.WriteLine(numsToText[firstNum] + " hundred and " + teensToText[thirdNum]);
+            }
+            else if (secondNum == 0)
+            {
+                if (thirdNum == 0)
+                {
+                    Console.WriteLine(numsToText[firstNum] + " hundred");
+                }
+                else
+                {
+                    Console.WriteLine("{0} {1}", numsToText[firstNum] + " hundred and", numsToText[thirdNum]);
+                }
+            }
+            else
+            {
+                if (thirdNum == 0)
+                {
+                    Console.WriteLine(numsToText[firstNum] + " hundred and " + tensToText[secondNum - 2]);
+                }
+                else
+                {
+                    Console.WriteLine("{0} {1} {2}", numsToText[firstNum] + " hundred and", tensToText[secondNum - 2], numsToText[thirdNum]);
+                }
+            }
+        }
     }
 }
